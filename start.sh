2bd2e479e00ec1b0654b676a94091417ae693a2f
Rocco Schulz <rocco.schulz@mimacom.com>
2023-03-07 13:26:04 +0100

    remove elk from start.sh, as it is only needed for the connect demo

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 if [ ! -f "docker-compose.yml" ]; then
   wget https://raw.githubusercontent.com/confluentinc/cp-all-in-one/7.3.0-post/cp-all-in-one/docker-compose.yml
 fi
-docker-compose  -f docker-compose.yml -f docker-compose-kcat.yml -f docker-compose-elk.yml -f docker-compose-akhq.yml  up -d
+docker-compose  -f docker-compose.yml -f docker-compose-kcat.yml -f docker-compose-akhq.yml  up -d
