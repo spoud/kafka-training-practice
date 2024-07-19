d4973c2f4bec8b102af273ef6ed594f47cab1e87
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2024-07-19 15:24:13 +0200

    fix: use all Docker compose files in the start and stop scripts

--- stop.sh
+++ stop.sh
@@ -3,4 +3,10 @@
 if [ ! -f "docker-compose.yml" ]; then
   wget https://raw.githubusercontent.com/confluentinc/cp-all-in-one/7.6.2-post/cp-all-in-one-kraft/docker-compose.yml
 fi
-docker-compose -f docker-compose.yml -f docker-compose-kcat.yml -f docker-compose-akhq.yml down
+docker-compose \
+  -f docker-compose.yml \
+  -f docker-compose-akhq.yml \
+  -f docker-compose-elk.yml \
+  -f docker-compose-flink.yml \
+  -f docker-compose-kcat.yml \
+  down
