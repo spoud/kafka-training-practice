fadb789b8ac2e6a5f54a7ca50622152f914419b3
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2024-07-19 15:18:57 +0200

    fix: update the URLs from where the compose file is downloaded
    The scripts were downloading different versions.
    The URL also pointed to a Kafka cluster with Zookeeper.

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [ ! -f "docker-compose.yml" ]; then
-  wget https://raw.githubusercontent.com/confluentinc/cp-all-in-one/7.6.1-post/cp-all-in-one/docker-compose.yml
+  wget https://raw.githubusercontent.com/confluentinc/cp-all-in-one/7.6.2-post/cp-all-in-one-kraft/docker-compose.yml
 fi
-docker-compose  -f docker-compose.yml -f docker-compose-kcat.yml -f docker-compose-akhq.yml  up -d
+docker-compose -f docker-compose.yml -f docker-compose-kcat.yml -f docker-compose-akhq.yml up -d
