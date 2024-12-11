69e2019e4c3ad234cdfed2fdc4b8dae8ce259a62
Horacio Lisdero Scaffino <hlisdero@users.noreply.github.com>
2024-12-11 21:30:43 +0100

    style: format files with Google Style

--- kafka-exercises/3.1-java-client/java-producer/src/main/java/org/example/ProducerApp.java
+++ kafka-exercises/3.1-java-client/java-producer/src/main/java/org/example/ProducerApp.java
@@ -4,10 +4,8 @@ package org.example;
  * Simple java producer
  *
  */
-public class ProducerApp
-{
-    public static void main( String[] args )
-    {
-        // TODO: implement the producer
-    }
+public class ProducerApp {
+  public static void main(String[] args) {
+    // TODO: implement the producer
+  }
 }
